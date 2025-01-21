d83a5cf611273dd19047b0e25417cba15d80dbaf
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-01-21 03:27:30 +0000

    solved

--- exercise/coffee_shop.rb
+++ exercise/coffee_shop.rb
@@ -7,7 +7,7 @@ class CoffeeShop
     @inventory = Inventory.new
   end
 
-  def self.lace_order(type, size)
+  def place_order(type, size)
     coffee = Coffee.new(type, size) 
     puts "📖 Placed order for a #{coffee.description}"  
     @orders << coffee 
@@ -16,7 +16,7 @@ class CoffeeShop
   def fulfill_orders
     puts "🔄 Fulfilling orders..."
 
-    orders.each do |coffee|
+    @orders.each do |coffee|
       if !@inventory.has_enough_supplies?(1)
         @inventory.use_supplies(1)
         puts "✅ Fulfilled order for #{coffee.description}"
@@ -25,10 +25,7 @@ class CoffeeShop
       end
     end
 
-    orders.clean_up
   end
-
-  private
 
   def clean_up
     puts "There are 0 orders remaining!"
